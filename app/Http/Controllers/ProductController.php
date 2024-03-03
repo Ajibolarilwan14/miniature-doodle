af44f22f16c0ecc50f640d7d715d50389bb14d72
Adebayo Rilwan Ajibola <ajibolarilwan14@gmail.com>
2024-03-04 00:31:53 +0100

    set up cache on product listing using Redis

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -6,26 +6,31 @@ use App\Models\Product;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Redis;
 use App\Http\Resources\ProductResource;
 use App\Http\Requests\ProductValidation;
 
 class ProductController extends BaseController
 {
-    public function index(Request $request): JsonResponse
-    {
-        $products = [];
+    public function index(Request $request)
+    {   
+        $products = Cache::many(['products']);
         $user = auth()->user();
 
-        if($request->query('limit'))
+        if(!is_null($products['products']))
         {
-            $products = Product::where('user_id', $user->id)->paginate((int) $request->query('limit'));
 
             return $this->sendResponse($products, "Products Retreived Successfully");
         }
 
-        $products = Product::where('user_id', $user->id)->get();
+            $products = Product::where('user_id', $user->id)->get();
+    
+            Cache::putMany(['products' => $products], 60);
+    
+            return $this->sendResponse($products, "Products Retreived Successfully");
 
-        return $this->sendResponse($products, "Products Retreived Successfully");
+        
 
     }
 
@@ -47,7 +52,16 @@ class ProductController extends BaseController
     {
         if($product->user_id !== auth()->user()->id) abort(Response::HTTP_FORBIDDEN, "you do not have permission to this resource");
         
-        return $this->sendResponse(new ProductResource($product), "Product Retrieved Successfully!");
+        $product_cache = Cache::get('product_' . $product->id);
+
+        if(isset($product_cache))
+        {
+            return $this->sendResponse(new ProductResource($product_cache), "Product Retrieved Successfully!");
+        }else{
+            Cache::set('product_' . $product->id, $product, 60);
+            
+            return $this->sendResponse(new ProductResource($product), "Product Retrieved Successfully!");
+        }
     }
 
     public function update(ProductValidation $request, Product $product): JsonResponse
@@ -59,9 +73,13 @@ class ProductController extends BaseController
         {
             $product->update($request->validated());
 
+            Cache::delete('product_' . $product->id);
+
         }else{
             return $this->sendError([], "The selected product category id does not exist");
         }
+        
+        Cache::set('product_' . $product->id, $product, 60);
         
         return $this->sendResponse([], "Product Edited Successfully");
     }
@@ -70,7 +88,10 @@ class ProductController extends BaseController
     {
         if($product->user_id !== auth()->user()->id) abort(Response::HTTP_FORBIDDEN, "you do not have permission to this resource");
 
+        Cache::delete('product_' . $product->id);
+        
         $product->delete();
+
 
         return $this->sendResponse([], "Product deleted successfully!");
     }
