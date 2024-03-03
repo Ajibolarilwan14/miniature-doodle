4b45c83ceceba7da08bb4497fcdf8c724fc54045
Adebayo Rilwan Ajibola <ajibolarilwan14@gmail.com>
2024-03-03 09:50:17 +0100

    implement authentication

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use App\Http\Requests\LoginRequest;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends BaseController
+{
+    public function __invoke(LoginRequest $request)
+    {
+        $user = User::where('email', $request->email)->first();
+
+        if(!($user) || (!(Hash::check($request->password, $user->password))))
+        {
+            return $this->sendError([], "Whoops, that credentials does not match our records!", Response::HTTP_UNPROCESSABLE_ENTITY);
+        }
+
+        return $this->sendResponse([
+            'token' => $user->createToken('miniaturedoodle20240303')->plainTextToken
+        ], "Login Successfully");
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ProductCategoryController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,13 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+Route::prefix('auth')->group(function() {
+    Route::post('/login', AuthController::class);
+});
+
+Route::prefix('product')->middleware(['auth:sanctum'])->group(function() {
+    Route::get('/create', [ProductCategoryController::class, 'index']);
+});
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
